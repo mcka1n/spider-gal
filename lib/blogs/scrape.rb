a7ccdbafd1f118d4896b9e6cd1bc5300fe7a3a2e
Ed Mckain <mckain.default@gmail.com>
2014-10-03 15:38:14 -0600

    add clean_product_name method on Scrape::Base

--- lib/blogs/scrape.rb
+++ lib/blogs/scrape.rb
@@ -20,6 +20,24 @@ module SpiderGirl
           # to be defined by child
         end
 
+        def clean_product_name sentence
+          #sentence = "levis 501 commuter jeans 30x32"
+          default_brand = "Browsy Brand"
+          words = sentence.split(' ')
+          result = {:product_name => sentence, :brand => default_brand}
+
+          words.each do |word|
+            matches = BrandCatalog.where("name LIKE ?", "%#{word}%")
+            if matches.count > 0
+              brand = matches.first
+              sentence.slice! brand
+              result = {:product_name => sentence, :brand => brand}
+              break
+            end
+          end
+
+          result
+        end
 
       end
     end
