798d58e7eeebacd6a5480b4ec9253ec9790ddb82
Ed Mckain <mckain.default@gmail.com>
2014-10-17 09:21:57 -0600

    separate atlantic pacific scrapper into rake + worker

---
+++ lib/workers/atlantic_pacific_product_worker.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+require 'open-uri'
+require 'rake'
+
+Bundler.require :default
+
+class AtlanticPacificProductWorker
+  include Sidekiq::Worker
+
+  def perform(link_to_scrape)
+    post_url = link_to_scrape
+    browser = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36"
+    product = Nokogiri::HTML(open(post_url, 'User-Agent' => browser))
+
+    SpiderGirl.logger.info "[spider-girl] @AP, Asking Shopsense for product."
+
+    # call shopstyle
+    result = SpiderGirl.ask_shopstyle_for product.at_css('title').content
+
+    # DO THE POST
+    CreateProductWorker.perform_async(result[:name],
+                                      result[:image_url],
+                                      result[:buy_link],
+                                      result[:original_price],
+                                      result[:current_price],
+                                      result[:brand],
+                                      origin_url)
+
+  end
+
+end
